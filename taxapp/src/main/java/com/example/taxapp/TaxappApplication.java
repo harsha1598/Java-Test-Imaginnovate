716cbb139b5de756fef534a2e4904162cd376e1a
harsha1598 <harshareddy1598@gmail.com>
2023-04-14 16:38:49 +0530

    Created Spring starter for the app

---
+++ taxapp/src/main/java/com/example/taxapp/TaxappApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxappApplication.class, args);
+	}
+
+}
